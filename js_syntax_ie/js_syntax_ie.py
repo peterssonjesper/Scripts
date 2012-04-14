60ada641458d16a239f771f8783fd79932ac31b7
Jesper Petersson <jesper@doubleint.com>
2012-04-14 16:56:56 +0200

    Fixed so that if no file is given on command line, an error is shown

--- js_syntax_ie/js_syntax_ie.py
+++ js_syntax_ie/js_syntax_ie.py
@@ -4,9 +4,7 @@ import sys, re, os
 SCAN_EXTENSIONS = ["js", "html", "php", "py", "rb"]
 
 def error_in_line(line):
-	if re.search(",}", line): # Check inside a line
-		return True
-	return False
+	return True if re.search(",}", line) else False# Check inside a line
 
 def search_file(filename):
 	try:
@@ -23,13 +21,9 @@ def search_file(filename):
 			print "Found possible error in file " + filename + " on line " + str(i+1)
 		elif i < len(content)-1:
 			joined_line = content[i] + content[i+1]
-			if error_in_line(joined_line) and not error_in_line(content[i+1]): # Check inside a line
+			if error_in_line(joined_line) and not error_in_line(content[i+1]): # Check between two lines
 				print "Found possible error in file " + filename + " between line " + str(i+1) + " and " + str(i+2)
 		i += 1
-
-	if len(sys.argv) != 2:
-		print "Usage: find.py file.js"
-		sys.exit(1)
 
 def rec_search(dir_name):
 	for f in os.listdir(dir_name):
@@ -37,6 +31,10 @@ def rec_search(dir_name):
 			rec_search(dir_name + "/" + f)
 		elif (dir_name + "/" + f).split(".")[-1].lower() in SCAN_EXTENSIONS:
 			search_file(dir_name + "/" + f)
+
+if len(sys.argv) != 2:
+	print "Usage: js_syntax_ie.py file.js"
+	sys.exit(1)
 
 if os.path.isdir(sys.argv[1]):
 	rec_search(sys.argv[1])
